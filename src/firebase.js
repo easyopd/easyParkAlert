032e7bf33b7b6f708cab267504bd07f3b5ba3654
easyopd <help.easyopd@gmail.com>
2025-01-25 21:53:12 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -22,6 +22,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log('Firebase Config:', firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
